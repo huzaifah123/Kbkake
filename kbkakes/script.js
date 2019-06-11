7f5f68e55a6d6ddc080825be0a40d9399ac558a6
huzaifah123 <huzaifah.m123@gmail.com>
2019-06-12 00:36:13 +0100

    Implementing website

---
+++ kbkakes/script.js
@@ -0,0 +1,25 @@
+function closeMenu() {
+  document.getElementById("navbar").style.height = "0%";
+}
+function openMenu() {
+  document.getElementById("navbar").style.height = "100%";
+}
+$(function() {
+  $("#portfolio").magnificPopup({
+    delegate: "a",
+    type: "image",
+    image: {
+      cursor: null,
+      titleSrc: "title"
+    },
+    gallery: {
+      enabled: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+      navigateByImgClick: true
+    }
+  });
+});
+// MDB Lightbox Init
+$(function() {
+  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
